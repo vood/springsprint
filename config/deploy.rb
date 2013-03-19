215b7a7732e09bb7074b076744db71e6d32108c7
vood <artem.vysotsky@gmail.com>
2013-03-19 14:36:11 +0300

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+
+set :application, "springsprint"
+set :repository, 'git@bitbucket.org:vood/springsprint.git'
+
+set :location, "78.47.197.109"
+
+set :scm, :git
+
+set :deploy_to, "/home/vood/#{application}"
+
+set :use_sudo, false
+
+set :deploy_via, :copy
+
+set :keep_releases, 10
+
+
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+server "78.47.197.109", :app, :web, :db, :primary => true
+
+set :user, "vood"
+
